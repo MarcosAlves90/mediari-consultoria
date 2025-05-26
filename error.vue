bd9d1021702e1bf5dec92c2a21d3f859bd0189b6
MarcosAlves90 <MarcosAlves25@proton.me>
2025-05-25 21:28:06 -0300

    Atualiza mensagens de erro e refatora props para maior clareza; ajusta .gitignore e melhora documentação das funções utilitárias

--- error.vue
+++ error.vue
@@ -4,7 +4,7 @@ import { useI18n } from 'vue-i18n';
 const { t, locale } = useI18n();
 
 const props = defineProps({
-    error: {
+    errorMessage: {
         type: Object,
         required: true,
     },
@@ -15,7 +15,7 @@ useSeoMeta({
 });
 
 defineOgImageComponent('Mediari', {
-    title: (props.error?.statusCode ?? '') + ' ' + t('error.title')
+    title: (props.errorMessage?.statusCode ?? '') + ' ' + t('error.title')
 })
 
 const { goTo } = useGoTo();
@@ -32,9 +32,9 @@ function goHome() {
         <div class="header flex items-center gap-1">
             <Icon class="logo text-[6rem] text-accent-color" name="my-icon:mediari-logo"
                 aria-label="Logotipo Mediari" />
-            <h1 class="text-[5rem] m-0">{{ props.error.statusCode }}</h1>
+            <h1 class="text-[5rem] m-0">{{ props.errorMessage.statusCode }}</h1>
         </div>
-        <p class="error-page__p" v-if="props.error.statusCode === 404">{{ t('error.not_found') }}</p>
+        <p class="error-page__p" v-if="props.errorMessage.statusCode === 404">{{ t('error.not_found') }}</p>
         <p class="error-page__p" v-else>{{ t('error.unexpected') }}</p>
         <button class="common-button" @click="goHome" :aria-label="t('error.back_home_aria')">
             {{ t('error.back_home') }}
