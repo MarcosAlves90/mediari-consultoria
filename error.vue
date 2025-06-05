0dfd412fda2c79b4acf3b8966dfaaf34f4bd33f3
MarcosAlves90 <MarcosAlves25@proton.me>
2025-06-05 19:50:52 -0300

    Refactors error handling and SEO meta updates
    Improves error handling by renaming and restructuring the error prop for clarity and type-safety. Updates SEO meta logic to ensure consistent behavior across components. Enhances error page layout with better styling and accessibility adjustments.

--- error.vue
+++ error.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import type { PropType } from 'vue';
 import { useGoTo } from '@/utils/useGoTo';
 import { useI18n } from 'vue-i18n';
+
 const { t, locale } = useI18n();
 
 const props = defineProps({
-    errorMessage: {
-        type: Object,
+    error: {
+        type: Object as PropType<{
+            statusCode: number;
+            statusMessage?: string;
+            message?: string;
+        }>,
         required: true,
     },
 });
@@ -15,8 +21,8 @@ useSeoMeta({
 });
 
 defineOgImageComponent('Mediari', {
-    title: (props.errorMessage?.statusCode ?? '') + ' ' + t('error.title')
-})
+    title: (props.error?.statusCode ?? '') + ' ' + t('error.title')
+});
 
 const { goTo } = useGoTo();
 
@@ -27,14 +33,13 @@ function goHome() {
 
 <template>
     <div
-        class="error-page flex flex-col justify-center items-center h-screen text-center bg-body-bg text-accent-color py-0 px-2 gap-1">
-        <div class="mesh-gradient-background"></div>
-        <div class="header flex items-center gap-1">
+        class="error-page flex flex-col justify-center items-center h-screen text-center bg-body-bg text-accent-color py-0 px-2 gap-1 relative">
+        <div class="mesh-gradient-background"></div><div class="header flex items-center gap-1">
             <Icon class="logo text-[6rem] text-accent-color" name="my-icon:mediari-logo"
                 aria-label="Logotipo Mediari" />
-            <h1 class="text-[5rem] m-0">{{ props.errorMessage.statusCode }}</h1>
+            <h1 class="text-[5rem] m-0">{{ props.error.statusCode }}</h1>
         </div>
-        <p class="error-page__p" v-if="props.errorMessage.statusCode === 404">{{ t('error.not_found') }}</p>
+        <p class="error-page__p" v-if="props.error.statusCode === 404">{{ t('error.not_found') }}</p>
         <p class="error-page__p" v-else>{{ t('error.unexpected') }}</p>
         <button class="common-button" @click="goHome" :aria-label="t('error.back_home_aria')">
             {{ t('error.back_home') }}
