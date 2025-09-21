b2bacb5e67cc419bf0d2331c452956958c4df13e
MarcosAlves90 <MarcosAlves25@proton.me>
2025-09-21 14:39:35 -0300

    Reorder socialAction section link in AppFooter

--- components/organisms/AppFooter.vue
+++ components/organisms/AppFooter.vue
@@ -36,9 +36,9 @@
     { label: t('navbar.home'), section: 'banner-section' },
     { label: t('navbar.services'), section: 'services-section' },
     { label: t('navbar.enterprise'), section: 'enterprise-section' },
-    { label: t('navbar.socialAction'), section: 'social-action-section' },
     { label: t('navbar.founder'), section: 'seo-section' },
     { label: t('navbar.team'), section: 'team-section' },
+    { label: t('navbar.socialAction'), section: 'social-action-section' },
     { label: t('navbar.contact'), section: 'contact-section' },
     { label: t('navbar.careers'), href: '/trabalhe-conosco' },
   ])
