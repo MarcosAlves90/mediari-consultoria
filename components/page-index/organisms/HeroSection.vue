d8df74a34a7cdbf35800df90c42248154ccb4a60
MarcosAlves90 <MarcosAlves25@proton.me>
2025-09-21 14:39:35 -0300

    Remove unused defineEmits import from HeroSection

--- components/page-index/organisms/HeroSection.vue
+++ components/page-index/organisms/HeroSection.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
   import { useI18n } from 'vue-i18n'
   import { useHeroTags } from '~/composables/page-index/useHeroTags'
-  import { defineEmits } from 'vue'
 
   const { t } = useI18n()
   const { heroTags } = useHeroTags()
