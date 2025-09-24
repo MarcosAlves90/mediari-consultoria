c17a56f46cc7006e8c0d81917c02e7e1e29ccdc6
MarcosAlves90 <MarcosAlves25@proton.me>
2025-09-24 13:37:05 -0300

    refactor(layout): move AdminNavbar/AdminFooter imports to <script setup> and adjust layout structure

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,23 +1,17 @@
-<template>
-  <div
-    class="admin-layout min-h-[calc(100vh-64px)] mt-[64px] bg-body-bg flex flex-col"
-  >
-    <!-- Admin Navbar -->
-    <AdminNavbar />
-
-    <!-- Main Content -->
-    <main class="flex-1 relative">
-      <div class="relative z-10">
-        <slot />
-      </div>
-    </main>
-
-    <!-- Admin Footer -->
-    <AdminFooter />
-  </div>
-</template>
-
 <script setup lang="ts">
   import AdminNavbar from '~/components/organisms/AdminNavbar.vue'
   import AdminFooter from '~/components/organisms/AdminFooter.vue'
 </script>
+
+<template>
+    <!-- Admin Navbar -->
+    <AdminNavbar />
+
+    <!-- Main Content -->
+    <main class="flex-1 relative admin-layout min-h-[calc(100vh-64px)] mt-[64px] bg-body-bg flex flex-col">
+      <slot />
+    </main>
+
+    <!-- Admin Footer -->
+    <AdminFooter />
+</template>
