62eef32b31f81994ea0d3c9085d98e5bcae7b191
MarcosAlves90 <MarcosAlves25@proton.me>
2025-09-25 00:22:46 -0300

    Refactor admin layout formatting for improved readability

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -4,14 +4,16 @@
 </script>
 
 <template>
-    <!-- Admin Navbar -->
-    <AdminNavbar />
+  <!-- Admin Navbar -->
+  <AdminNavbar />
 
-    <!-- Main Content -->
-    <main class="flex-1 relative admin-layout min-h-[calc(100vh-64px)] mt-[64px] bg-body-bg flex flex-col">
-      <slot />
-    </main>
+  <!-- Main Content -->
+  <main
+    class="flex-1 relative admin-layout min-h-[calc(100vh-64px)] mt-[64px] bg-body-bg flex flex-col"
+  >
+    <slot />
+  </main>
 
-    <!-- Admin Footer -->
-    <AdminFooter />
+  <!-- Admin Footer -->
+  <AdminFooter />
 </template>
