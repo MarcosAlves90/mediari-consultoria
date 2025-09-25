42e66332e6d293b7042474a2b3add3d53f3fe65b
MarcosAlves90 <MarcosAlves25@proton.me>
2025-09-25 17:56:33 -0300

    feat: add index barrel file for molecules components

---
+++ components/page-admin/molecules/index.ts
@@ -0,0 +1,6 @@
+export { default as CandidateDetails } from './CandidateDetails.vue';
+export { default as CandidatePersonalInfo } from './CandidatePersonalInfo.vue';
+export { default as CandidateExperience } from './CandidateExperience.vue';
+export { default as CandidateCoverLetter } from './CandidateCoverLetter.vue';
+export { default as CandidateTestResults } from './CandidateTestResults.vue';
+export { default as ProfileTestResults } from './ProfileTestResults.vue';
